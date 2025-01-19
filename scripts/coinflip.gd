039862abf7332af0507c248f582851b1572e49ab
Albert Margaryan <albertmargaryan2004@gmail.com>
2025-01-18 19:39:36 -0500

    coinflip money working

--- scripts/coinflip.gd
+++ scripts/coinflip.gd
@@ -5,6 +5,7 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Label3.text = "Your money: \n" + str(globals.coins) + " coins."
 	pass # Replace with function body.
 
 
@@ -14,8 +15,11 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	if globals.coins < 10:
+		pass
 	$AnimatedSprite2D.play()
 	$Timer.start()
+	globals.coins -= 10
 
 func _on_timer_timeout() -> void:
 	var my_random_number = rng.randi_range(0, 1)
@@ -25,13 +29,17 @@ func _on_timer_timeout() -> void:
 	if my_random_number == 1:
 		print("Heads")
 		$Label.text = "Heads"
+		globals.coins += 20
 		$AnimatedSprite2D.frame = 0
 		$AnimatedSprite2D.pause()
 	
 	elif my_random_number == 0:
 		print("Tails")
 		$Label.text = "Tails"
+		globals.coins -= 5
 		$AnimatedSprite2D.frame = 4
 		$AnimatedSprite2D.pause()
+	_update_coins_label()
 
-	
+func _update_coins_label() -> void:
+	$Label3.text = "Your money \n" + str(globals.coins) + " coins."
--- scripts/minigames.gd
+++ scripts/minigames.gd
@@ -6,8 +6,12 @@ func _on_back_button_pressed() -> void:
 
 
 func _on_button_pressed() -> void:
-	scene_switcher.switch_scene("res://scenes/main.tscn")
+	scene_switcher.switch_scene("res://scenes/flappy_mini.tscn")
 
 
 func _on_button_5_pressed() -> void:
 	scene_switcher.switch_scene("res://scenes/main.tscn")
+
+
+func _on_button_4_pressed() -> void:
+	scene_switcher.switch_scene("res://scenes/minigames/coinflip.tscn")
